bc2a50a9bdf3249ec29ced142a1a4edb9ce1cf94
ingchrist <cdjinguet@gmail.com>
2024-07-08 22:45:23 +0100

    ok

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -94,3 +94,10 @@ class BasicAuth(Auth):
                     if email is not None:
                         return self.user_object_from_credentials(email, pword)
         return
+         
+         
+          
+          
+
+         
+
